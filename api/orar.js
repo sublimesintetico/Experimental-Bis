e7c606c3d91812477b2da37a8ef8df9b8f14f62e
sublimesintetico <tgganm@gmail.com>
2026-04-11 14:22:52 +0200

    Testing Mail

--- api/orar.js
+++ api/orar.js
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            from: "orar@sublimesintetico.com",
-            to: "sinteticosublime@gmail.com",
+            from: "onboarding@resend.dev",
+            to: "tgganm@gmail.com",
             subject: `Oración — ${fecha}`,
             html: `
                 <p><strong>Texto:</strong> ${texto}</p>
